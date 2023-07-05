f88d60bcb30e9ad847c635dfa48d302e60e7fb7f
minischoolX <104305605+minischoolX@users.noreply.github.com>
2023-07-05 18:16:07 +0530

    Update MainActivity.java

--- app/src/main/java/com/example/app/MainActivity.java
+++ app/src/main/java/com/example/app/MainActivity.java
@@ -7,32 +7,50 @@ import android.webkit.WebSettings;
 import android.webkit.WebView;
 
 public class MainActivity extends Activity {
+  private WebView mWebView;
 
-    private WebView mWebView;
+  @Override
+  @SuppressLint("SetJavaScriptEnabled")
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.activity_main);
+    mWebView = findViewById(R.id.activity_main_webview);
+    WebSettings webSettings = mWebView.getSettings();
+    webSettings.setJavaScriptEnabled(true);
+    mWebView.setWebViewClient(new getsetGoClient());
+    mWebView.addJavascriptInterface(new JavaScriptInterface(), "Android");
+    mWebView.loadUrl("https://google.com");
+}
 
-    @Override
-    @SuppressLint("SetJavaScriptEnabled")
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        mWebView = findViewById(R.id.activity_main_webview);
-        WebSettings webSettings = mWebView.getSettings();
-        webSettings.setJavaScriptEnabled(true);
-        mWebView.setWebViewClient(new MyWebViewClient());
-
-        // REMOTE RESOURCE
-        // mWebView.loadUrl("https://example.com");
-
-        // LOCAL RESOURCE
-        // mWebView.loadUrl("file:///android_asset/index.html");
+  private class JavaScriptInterface {
+    @JavascriptInterface
+    public String GM_getValue(String key, String value) {
+	  String v = valueStore.getValue(key);
+	  return (v != null) ? v : value;
+	}
+        
+    @JavascriptInterface
+	public void GM_setValue(String key, String value) {
+	  valueStore.setValue(key, value);
+	}
+        
+    @JavascriptInterface
+    public void GM_registerMenuCommand(String caption, String configPage) {
+      mWebView.loadUrl(configPage);
     }
 
-    @Override
-    public void onBackPressed() {
-        if(mWebView.canGoBack()) {
-            mWebView.goBack();
-        } else {
-            super.onBackPressed();
-        }
+    @JavascriptInterface
+    public void GM_openInWindow(String url) {
+      mWebView.loadUrl(url);
     }
+  }
+
+  @Override
+  public void onBackPressed() {
+    if(mWebView.canGoBack()) {
+      mWebView.goBack();
+    } else {
+      super.onBackPressed();
+    }
+  }
 }
