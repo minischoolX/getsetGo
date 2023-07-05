792e96d272051f14ba322f1e9d9e667b61bbd200
minischoolX <104305605+minischoolX@users.noreply.github.com>
2023-07-05 19:02:04 +0530

    Update MainActivity.java

--- app/src/main/java/com/example/app/MainActivity.java
+++ app/src/main/java/com/example/app/MainActivity.java
@@ -6,9 +6,21 @@ import android.os.Bundle;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
 
+import android.webkit.JavascriptInterface;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.webkit.WebViewClient;
+
+import android.graphics.Bitmap;
+import java.io.IOException;
+import java.io.InputStream;
+import android.widget.Toast;
+
 public class MainActivity extends Activity {
   private WebView mWebView;
-
+  private ValueStore valueStore;
+	
   @Override
   @SuppressLint("SetJavaScriptEnabled")
   protected void onCreate(Bundle savedInstanceState) {
@@ -17,7 +29,45 @@ public class MainActivity extends Activity {
     mWebView = findViewById(R.id.activity_main_webview);
     WebSettings webSettings = mWebView.getSettings();
     webSettings.setJavaScriptEnabled(true);
-    mWebView.setWebViewClient(new getsetGoClient());
+    //mWebView.setWebViewClient(new getsetGoClient());
+    mWebView.setWebViewClient(new WebViewClient() {
+      @Override
+      public void onPageStarted(WebView view, String url, Bitmap favicon) {
+        super.onPageStarted(view, url, favicon);
+        if (url.matches("^(https?://(www|m)\\.youtube\\.com/.*)|(https?://.*\\.youtube-nocookie\\.com/embed/.*)|(https?://youtube\\.googleapis\\.com/embed/.*)|(https?://raingart\\.github\\.io/options\\.html.*)$") &&
+          !url.matches("^(https?://.*\\.youtube\\.com/.*\\.xml.*)|(https?://.*\\.youtube\\.com/error.*)|(https?://music\\.youtube\\.com/.*)|(https?://accounts\\.youtube\\.com/.*)|(https?://studio\\.youtube\\.com/.*)|(https?://.*\\.youtube\\.com/redirect\\?.*)$")) {
+        
+        // Inject JavaScript code from the assets folder
+          try {
+            InputStream inputStream = getAssets().open("script.js");
+            int size = inputStream.available();
+            byte[] buffer = new byte[size];
+            inputStream.read(buffer);
+            inputStream.close();
+            String script = new String(buffer);
+
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
+              view.evaluateJavascript(script, null);
+            } else {
+              view.loadUrl("javascript:\n" + script);
+            }
+//            view.evaluateJavascript(script, null);
+            Toast.makeText(MainActivity.this, "javascript injected: " + url, Toast.LENGTH_SHORT).show();
+
+          } catch (IOException e) {
+            Toast.makeText(MainActivity.this, "javascript didn't inject ", Toast.LENGTH_SHORT).show();
+            e.printStackTrace();
+          }
+        }
+      }
+
+      @Override
+      public void onPageFinished(WebView view, String url) {
+      
+      }
+
+    });
+
     mWebView.addJavascriptInterface(new JavaScriptInterface(), "Android");
     mWebView.loadUrl("https://google.com");
 }
