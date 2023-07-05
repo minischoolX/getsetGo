5c8db1aba90ada22c26e2d7267def3fe4b30e6b2
minischoolX <104305605+minischoolX@users.noreply.github.com>
2023-07-05 17:54:56 +0530

    Update ValueStoreSQLite.java

--- app/src/main/java/com/example/app/ValueStoreSQLite.java
+++ app/src/main/java/com/example/app/ValueStoreSQLite.java
@@ -7,52 +7,50 @@ import android.database.sqlite.SQLiteOpenHelper;
 
 public class ValueStoreSQLite implements ValueStore {
 
-	private Context context;
-	private ValueDbHelper dbHelper;
+private Context context;
+private ValueDbHelper dbHelper;
   
   @Override
-	public String getValue(String name) {
-		if (dbHelper == null) {
-			Log.e(TAG, "Cannot get value (database not available)");
-			return null;
-		}
-		return dbHelper.selectValue(name);
-	}
+  public String getValue(String key) {
+    if (dbHelper == null) {
+      return null;
+    }
+    return dbHelper.selectValue(key);
+  }
 
-	@Override
-	public void setValue(String name, String value) {
-		if (dbHelper == null) {
-			Log.e(TAG, "Cannot set value (database not available)");
-			return;
-		}
-		dbHelper.updateOrInsertValue(name, value);
-	}
+  @Override
+  public void setValue(String key, String value) {
+    if (dbHelper == null) {
+      return;
+    }
+    dbHelper.updateOrInsertValue(key, value);
+  }
 
-	public ValueStoreSQLite(Context context) {
-		this.context = context;
-	}
+  public ValueStoreSQLite(Context context) {
+    this.context = context;
+  }
 
   public synchronized void open() {
-		if (dbHelper != null) {
-			return;
-		}
-		dbHelper = new ValueDbHelper(context);
-	}
+    if (dbHelper != null) {
+      return;
+    }
+    dbHelper = new ValueDbHelper(context);
+  }
 
-	public synchronized void close() {
-		dbHelper.close();
-		dbHelper = null;
-	}
+  public synchronized void close() {
+    dbHelper.close();
+    dbHelper = null;
+  }
 
-	private static class ValueDbHelper extends SQLiteOpenHelper {
+  private static class ValueDbHelper extends SQLiteOpenHelper {
 
     private static final int DB_SCHEMA_VERSION_2 = 2;
-		private static final int DB_VERSION = DB_SCHEMA_VERSION_2;
+    private static final int DB_VERSION = DB_SCHEMA_VERSION_2;
 
-		private static final String DB = "valuestore";
+    private static final String DB = "valuestore";
 
     private static final String CREATE_VALUE_STORE_TABLE =
-        "CREATE TABLE " + VALUE_STORE +
+      "CREATE TABLE " + VALUE_STORE +
          "(" +
          COLUMN_KEY + " TEXT PRIMARY KEY," +
          COLUMN_VALUE + " TEXT" +
@@ -61,13 +59,13 @@ public class ValueStoreSQLite implements ValueStore {
     private SQLiteDatabase db;
 
     public ValueDbHelper(Context context) {
-			super(context, DB, null, DB_VERSION);
-			db = getWritableDatabase();
-			db.execSQL("PRAGMA foreign_keys = ON;");
-		}
+      super(context, DB, null, DB_VERSION);
+      db = getWritableDatabase();
+      db.execSQL("PRAGMA foreign_keys = ON;");
+    }
 
     @Override
-		public void onCreate(SQLiteDatabase db) {
+    public void onCreate(SQLiteDatabase db) {
       db.execSQL(CREATE_VALUE_STORE_TABLE);
     }
 
@@ -76,10 +74,10 @@ public class ValueStoreSQLite implements ValueStore {
         // Handle database upgrades if needed
     }
     
-		public String selectValue(String name) {
+    public String selectValue(String key) {
       String[] columns = {COLUMN_VALUE};
       String selection = COLUMN_KEY + "=?";
-      String[] selectionArgs = { name };
+      String[] selectionArgs = {key};
       Cursor cursor = db.query(VALUE_STORE, columns, selection, selectionArgs, null, null, null);
       String value = null;
       if (cursor.moveToFirst()) {
@@ -87,11 +85,11 @@ public class ValueStoreSQLite implements ValueStore {
       }
       cursor.close();
       return value;
-		}
+    }
 
-		public void updateOrInsertValue(String name, String value) {
+    public void updateOrInsertValue(String key, String value) {
       String selection = COLUMN_KEY + "=?";
-      String[] selectionArgs = {name};
+      String[] selectionArgs = {key};
       ContentValues fields = new ContentValues();
       fields.put(COLUMN_KEY, key);
       fields.put(COLUMN_VALUE, value);
@@ -104,6 +102,6 @@ public class ValueStoreSQLite implements ValueStore {
           db.insert(VALUE_STORE, null, fields);
         }
       cursor.close();      
-		}
+    }
   }
 }
