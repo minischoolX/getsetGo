604c77219ca5f1125b15b0786fb127089f095e82
minischoolX <104305605+minischoolX@users.noreply.github.com>
2023-07-05 17:46:43 +0530

    Update ValueStore.java

--- app/src/main/java/com/example/app/ValueStore.java
+++ app/src/main/java/com/example/app/ValueStore.java
@@ -2,22 +2,7 @@ package com.example.app;
 
 public interface ValueStore {
 
-	/**
-	 * Gets a value identified by its name.
-	 * 
-	 * @param name
-	 *            the key
-	 * @return a String value or null if none found
-	 */
-    String getValue(String name);
+    String getValue(String key);
 
-	/**
-	 * Sets a name-value pair.
-	 * 
-	 * @param name
-	 *            the key
-	 * @param value
-	 *            its value
-	 */
-    void setValue(String name, String value);
+    void setValue(String key, String value);
 }
